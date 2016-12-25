894f67001871974742299aabe456f0c31dd2fbd3
craus <craus902@gmail.com>
2016-12-26 00:19:25 +0400

    models, names list

---
+++ Assets/Scripts/Models/Game.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Game {
+    public List<Resource> resources = new List<Resource>();
+}
---
+++ Assets/Scripts/Models/Resource.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Resource {
+    public double weight;
+    public string name;
+    public Sprite image;
+    public Color color;
+}
