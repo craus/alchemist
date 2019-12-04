4f7863a87351e14f328f74c43be4c4ee323110fb
Igor Kachkovsky <kachkovsky-noreply-spam@yandex.ru>
2019-12-04 21:04:47 +0400

    remove sliders interaction

--- Assets/Scripts/SliderEventHandler.cs
+++ Assets/Scripts/SliderEventHandler.cs
@@ -8,12 +8,7 @@ using System;
 
 public class SliderEventHandler {
 
-    const int SLIDER_INT_MULTIPLIER = 100000;
-    const int SLIDER_INT_MAX_VALUE = SLIDER_INT_MULTIPLIER;
-
     public List<Slider> Sliders = new List<Slider>();
-
-    bool processing;
 
     public void AddSlider(Slider slider) {
         Sliders.Add(slider);
@@ -23,29 +18,11 @@ public class SliderEventHandler {
         Sliders.Remove(slider);
     }
 
-    private int sliderIntValue(Slider slider) {
-        return (int)(slider.value * SLIDER_INT_MULTIPLIER);
-    }
-
     public void OnValueChanged(Slider current) {
-        if (processing) {
-            return;
-        }
-        processing = true;
-        for (int summary = Sliders.Sum(s => sliderIntValue(s)); summary > SLIDER_INT_MAX_VALUE; summary = Sliders.Sum(s => sliderIntValue(s))) {
-            List<Slider> list = Sliders.FindAll(s => sliderIntValue(s) > 0);
-            list.Remove(current);
-            int min = sliderIntValue(list.OrderBy(s => sliderIntValue(s)).First());
-            int reduce = Math.Min((summary - SLIDER_INT_MAX_VALUE) / list.Count, min);
-            if (reduce == 0) {
-                list[0].value = Math.Max(0, list[0].value - (summary - SLIDER_INT_MAX_VALUE) / (float)SLIDER_INT_MAX_VALUE);
-                //Debug.Log("reduce 0");
-            } else {
-                list.ForEach(s => s.value -= (float)reduce / SLIDER_INT_MULTIPLIER);
-            }
-        }
-        processing = false;
+        onSlidersSettingFinished();
     }
 
-
+    public void onSlidersSettingFinished() {
+        GameManager.instance.GameStateChanged();
+    }
 }
